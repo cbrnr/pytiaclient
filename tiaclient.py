0500496106a973852573ed1a72f76a58da684f18
Clemens Brunner <clemens.brunner@tugraz.at>
2012-01-31 17:14:51 -0800

    Started implementing the data thread.

--- tiaclient.py
+++ tiaclient.py
@@ -3,22 +3,23 @@ import threading
 from lxml import etree
 
 class TIAClient(object):
-    """Provides a client for the TIA network protocol (version 1.0)."""
+    """Client for the TIA network protocol (version 1.0)."""
     
     def __init__(self):
         self._sock = None
+        self._sock_data = None
         self._metainfo = {"subject": None, "masterSignal": None, "signal": []}
+        self._thread_running = False  # State of the data thread
         self._buffer = []
     
     def connect(self, host, port):
         """Connects to server on host:port."""
-        if self._sock != None:  # Socket already exists
+        if self._sock != None:
             raise TIAError("Connection already established.")
-        
         try:
-            self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create socket
+            self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self._sock.settimeout(2)
-            self._sock.connect((host, port))  # Connect to host:port
+            self._sock.connect((host, port))
         except socket.error:
             self._sock = None
             raise TIAError("Cannot establish connection with server.")
@@ -35,16 +36,14 @@ class TIAClient(object):
         """Returns True if server supports the protocol version implemented by this client."""
         if self._sock == None:
             raise TIAError("No connection established.")
-        
-        try:    
+        try:
             self._sock.sendall("TiA 1.0\nCheckProtocolVersion\n\n")
-            tia_version = self._recv_until()  # Contains "TiA 1.0\n"
-            status = self._recv_until()  # Contains "OK" or "Error"
+            tia_version = self._recv_until().strip()  # Contains "TiA 1.0"
+            status = self._recv_until().strip()  # Contains "OK" or "Error"
             self._sock.recv(1)
         except socket.error, EOFError:
-            raise TIAError("Could not check protocol version.")
-        
-        if status.strip() == "OK":
+            raise TIAError("Checking protocol version failed.")
+        if status == "OK":
             return True
         else:
             return False
@@ -53,16 +52,15 @@ class TIAClient(object):
         """Retrieves meta information from the server."""
         if self._sock == None:
             raise TIAError("No connection established.")
-        
         try:
             self._sock.sendall("TiA 1.0\nGetMetaInfo\n\n")
-            tia_version = self._recv_until().strip()  # Contains "TiA 1.0\n" (remove trailing "\n")
-            msg = self._recv_until().strip()  # Contains "TiA 1.0\n"  # Contains "MetaInfo\n" (remove trailing "\n")
-            msg = self._recv_until().strip()  # Contains "Content-Length:xxx\n", where xxx is the number of bytes that follow (remove trailing "\n")
+            tia_version = self._recv_until().strip()  # Contains "TiA 1.0"
+            msg = self._recv_until().strip()  # Contains "MetaInfo"
+            msg = self._recv_until().strip()  # Contains "Content-Length:xxx", where "xxx" is the number of bytes that follow
             content_len = int(msg.split(":")[-1])
             xml_string = self._sock.recv(content_len + 1).strip()  # There is one extra "\n" at the end of the message
         except socket.error, EOFError:
-            raise TIAError("Could not receive metainfo.")
+            raise TIAError("Receiving metainfo failed.")
 
         xml = etree.fromstring(xml_string)
         self._metainfo = {"subject": None, "masterSignal": None, "signal": []}
@@ -75,41 +73,28 @@ class TIAClient(object):
             self._metainfo["signal"][index]["channels"] = []  # List of channels
             for channel in signal.findall("channel"):
                 self._metainfo["signal"][index]["channels"].append(channel.attrib)
-        
-    def get_data_connection(self, connection):
-        """Creates a data connection via TCP or UDP.
-             Input:  connection: "TCP" or "UDP"
-             Output: port number of new data connection
-        """
-        if self._sock == None:
-            raise TIAError("No connection established.")
-        if connection != "TCP" and connection != "UDP":
-            raise TIAError("Connection must be either TCP or UDP.")
-        
-        try:
-            self._sock.sendall("TiA 1.0\nGetDataConnection: " + connection + "\n\n")
-            tia_version = self._recv_until()  # Contains "TiA 1.0\n"
-            port = self._recv_until()  # Contains "OK" or "Error"
-            self._sock.recv(1)
-        except socket.error, EOFError:
-            raise TIAError("Could not get port of new data connection.")
-        
-        if port.find("Error -- Target and remote subnet do not match!") != -1:
-            raise TIAError("Target and remote subnets do not match for UDP data connection.")
-        else:
-            return int(port.split(":")[-1].strip())
     
-    def start_data(self):
+    def start_data(self, connection="TCP"):
         """Starts data transmission."""
-        # TODO: Start receiving data into buffer
-        # Calls _get_data_worker, which runs in a new thread and collects data from the
-        # socket buffer and writes it into the internal buffer.
-        
+        # TODO: Check that socket exists
+        port = self._get_data_connection("TCP")  # TODO: For now, only TCP data connections are supported
+        if self._sock_data != None:
+            raise TIAError("Data connection already established.")
+        try:
+            self._sock_data = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self._sock_data.settimeout(2)
+            self._sock_data.connect((self._sock.getpeername()[0], port))  # Connect to same host, but new port
+        except socket.error:
+            self._sock_data = None
+            raise TIAError("Cannot establish data connection.")
+        self._thread_running = True
+        t = threading.Thread(target=self._get_data)
+        t.start()
     
     def stop_data(self):
         """Stops data transmission."""
-        # TODO: Stop receiving data
-        # Ends _get_data_worker.
+        self._thread_running = False
+        self._sock_data.close()
     
     def get_state_connection(self):
         """Creates a state connection."""
@@ -118,7 +103,7 @@ class TIAClient(object):
     def get_data_chunk(self):
         # Returns the internal buffer.
         pass
-    
+
     def _recv_until(self, suffix="\n"):
         """Reads from socket until the character suffix is in the stream."""
         msg = ""
@@ -128,9 +113,32 @@ class TIAClient(object):
                 raise EOFError("Socket closed before receiving the delimiter.")
             msg += data
         return msg
-        
+
+    def _get_data_connection(self, connection):
+        """Returns the port number of the new data connection."""
+        if self._sock == None:
+            raise TIAError("No connection established.")
+        if connection != "TCP" and connection != "UDP":
+            raise TIAError("Data connection must be either TCP or UDP.")
+        try:
+            self._sock.sendall("TiA 1.0\nGetDataConnection: " + connection + "\n\n")
+            tia_version = self._recv_until().strip()  # Contains "TiA 1.0"
+            port = self._recv_until().strip()
+            self._sock.recv(1)
+        except socket.error, EOFError:
+            raise TIAError("Could not get port of new data connection.")
+
+        if port.find("Error -- Target and remote subnet do not match!") != -1:
+            raise TIAError("Target and remote subnets do not match for UDP data connection.")
+        else:
+            return int(port.split(":")[-1])
+
     def _get_data(self):
-        pass
+        print "Thread starting..."
+        while self._thread_running:
+            # TODO: Get all available data from socket and write it into self._buffer
+            pass
+        print "Thread ending..."
 
 
 class TIAError(Exception):
