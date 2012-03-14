c58c3dee16495f7c50bbc0bcaab1bceeabdfce24
Clemens Brunner <clemens.brunner@tugraz.at>
2012-03-14 15:16:36 -0700

    Decoding of data in a separate thread is now working.

--- tiaclient.py
+++ tiaclient.py
@@ -1,10 +1,15 @@
 import socket
 import threading
 import struct
+import time  # Only needed for test program
 from lxml import etree
+
+# TODO: Include logger
+# TODO: Include unit tests?
 
 SOCKET_TIMEOUT = 2  # Socket timeout (in seconds)
 TIA_VERSION = 1.0
+FIXED_HEADER_SIZE = 33  # Fixed header size (in bytes)
 
 class TIAClient(object):
     """Client for the TIA network protocol (version 1.0)."""
@@ -12,7 +17,6 @@ class TIAClient(object):
     def __init__(self):
         self._sock = None
         self._sock_data = None
-        self._metainfo = {"subject": None, "masterSignal": None, "signal": []}
         self._thread_running = False
         self._buffer = []
     
@@ -29,7 +33,7 @@ class TIAClient(object):
             raise TIAError("connect(): Cannot establish control connection (server might be down).")
         if not self._check_protocol():  # Check if protocol is supported by server
             raise TIAError("connect(): Protocol version {} not supported by server.".format(TIA_VERSION))
-        self._get_metainfo()  # Get meta information
+        self._get_metainfo()
     
     def close(self):
         """Closes control connection to server."""
@@ -50,12 +54,11 @@ class TIAClient(object):
         try:
             port = self._get_data_connection("TCP")
             self._sock_data = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self._sock_data.settimeout(2)
+            self._sock_data.settimeout(SOCKET_TIMEOUT)
             self._sock_data.connect((self._sock.getpeername()[0], port))  # Connect to same host, but new port
         except socket.error:
             self._sock_data = None
             raise TIAError("start_data(): Cannot establish data connection.")
-        self._thread_running = True
         try:
             self._sock.sendall("TiA {}\nStartDataTransmission\n\n".format(TIA_VERSION))
             tia_version = self._recv_until().strip()
@@ -65,27 +68,15 @@ class TIAClient(object):
             raise TIAError("start_data(): Starting data transmission failed.")
         if status != "OK":
             raise TIAError("start_data(): Starting data transmission failed.")
-        #t = threading.Thread(target=self._get_data)
-        #t.start()
-        data = self._get_data()  # FIXME: This replaces the thread, just for debugging purposes
+        self._thread_running = True
+        self.t = threading.Thread(target=self._get_data)
+        self.t.start()
     
     def stop_data(self):
         """Stops data transmission."""
-        self._thread_running = False
-        if self._sock == None:
-            raise TIAError("stop_data(): Control connection to server not established.")
-        if self._sock_data == None:
-            raise TIAError("stop_data(): No data connection established.")
-        try:
-            self._sock.sendall("TiA {}\nStopDataTransmission\n\n".format(TIA_VERSION))
-            tia_version = self._recv_until().strip()
-            status = self._recv_until().strip()
-            print status
-            self._sock.recv(1)
-        except socket.error, EOFError:
-            raise TIAError("stop_data(): Stopping data transmission failed.")
-        self._sock_data.close()
-        self._sock_data = None
+        if self._thread_running:
+            self._thread_running = False
+            self.t.join()
     
     def get_data_chunk(self):
         """Returns the data buffer and clears it."""
@@ -93,6 +84,7 @@ class TIAClient(object):
 
     def get_state_connection(self):
         """Creates a state connection."""
+        pass
         # TODO: This method should probably be private. It should run in a separate thread and just receives state messages.
 
     def _check_protocol(self):
@@ -119,7 +111,7 @@ class TIAClient(object):
             raise TIAError("_get_metainfo(): Receiving meta information failed (server might be down).")
         try:
             xml = etree.fromstring(xml_string)
-        except XMLSyntaxError:
+        except etree.XMLSyntaxError:
             raise TIAError("_get_metainfo(): Error while parsing XML meta information (syntax error).")
         self._metainfo = {"subject": None, "masterSignal": None, "signal": []}
         if xml.find("subject") is not None:
@@ -159,16 +151,39 @@ class TIAClient(object):
             return int(port.split(":")[-1])
 
     def _get_data(self):
-        print "Thread starting..."
         while self._thread_running:
-            # TODO: Get all available data from socket and write it into self._buffer
-            (d_version, d_size, d_flags, d_id, d_number, d_timestamp) = struct.unpack("<BIIQQQ", self._sock_data.recv(33))
+            (d_version, d_size, d_flags, d_id, d_number, d_timestamp) = struct.unpack("<BIIQQQ", self._sock_data.recv(FIXED_HEADER_SIZE))
             n_signals = bin(d_flags).count("1")
+            var_header_size = 4 * n_signals
             
-            break
-                
-        print "Thread ending..."
-        return 1
+            # FIXME: I don't know if these values are needed at all; I could just skip over the variable header
+            # FIXME: After all, these values are available in the meta information!
+            n_channels = []
+            block_size = []
+            for signals in range(n_signals):
+                tmp = struct.unpack("<H", self._sock_data.recv(2))
+                n_channels.append(tmp[0])
+            for signals in range(n_signals):
+                tmp = struct.unpack("<H", self._sock_data.recv(2))
+                block_size.append(tmp[0])
+            
+            for signal in range(n_signals):  # Read signal blocks
+                for channel in range(n_channels[signal]):
+                    for sample in range(block_size[signal]):
+                        data = struct.unpack("<f", self._sock_data.recv(4))
+                        print "Signal block {}, channel {}, sample {}: {}".format(signal + 1, channel + 1, sample + 1, data[0])
+                        # TODO: Write data in buffer
+                        
+        # Stop data transmission        
+        try:
+            self._sock.sendall("TiA {}\nStopDataTransmission\n\n".format(TIA_VERSION))
+            tia_version = self._recv_until().strip()
+            status = self._recv_until().strip()
+            self._sock.recv(1)
+        except socket.error, EOFError:
+            raise TIAError("stop_data(): Stopping data transmission failed.")
+        self._sock_data.close()
+        self._sock_data = None
 
 
 class TIAError(Exception):
@@ -179,6 +194,6 @@ if __name__ == "__main__":
     client = TIAClient()
     client.connect("137.110.244.73", 9000)
     client.start_data()
+    time.sleep(2)
     client.stop_data()
-    client.start_data()
     client.close()
