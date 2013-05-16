5cb28c9599011ff5635d9c98e0ce2c75b21da702
Clemens Brunner <clemens.brunner@tugraz.at>
2013-05-16 15:39:41 +0200

    Added license.

--- tiaclient.py
+++ tiaclient.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python2
+
+# Copyright 2013 by Clemens Brunner
+
+# This file is part of pytiaclient.
+# 
+# Pytiaclient is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Pytiaclient is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with pytiaclient. If not, see <http://www.gnu.org/licenses/>.
+
+
 import socket
 import threading
 import struct
@@ -215,6 +235,7 @@ class TIAError(Exception):
 
 
 # Helper functions
+# TODO: maybe outsource helper functions to separate .py file
 def recv_until(sock, suffix="\n"):
     """Reads from socket until the character suffix is in the stream."""
     msg = ""
@@ -240,6 +261,6 @@ if __name__ == "__main__":
     client.connect("129.27.145.32", 9000)
     client.start_data()
     raw_input("Press Enter to quit.")
-    data = client.get_data_chunk()  # TODO: get mapping of indices
+    data = client.get_data_chunk()
     client.stop_data()
     client.close()
