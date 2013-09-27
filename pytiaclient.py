263456c899051f5ca2c77b37334fecc3eec0f6b9
Clemens Brunner <clemens.brunner@tugraz.at>
2013-09-27 14:36:18 +0200

    Updated licence header.

--- pytiaclient.py
+++ pytiaclient.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python2
 
-# Copyright 2013 by Clemens Brunner
+# Copyright 2013 by Clemens Brunner.
 
-# This file is part of pytiaclient.
-# 
-# Pytiaclient is free software: you can redistribute it and/or modify
+# This file is part of Sigviewr.
+#
+# Sigviewr is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
-# Pytiaclient is distributed in the hope that it will be useful,
+#
+# Sigviewr is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
-# along with pytiaclient. If not, see <http://www.gnu.org/licenses/>.
+# along with Sigviewr. If not, see <http://www.gnu.org/licenses/>.
 
 
 import socket
